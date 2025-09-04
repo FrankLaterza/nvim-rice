f28cbd83f614306e0273be990ad0bd532a8a287a
FrankLaterza <franklaterza@gmail.com>
2025-09-04 15:34:07 +0100

    ssh copy paste

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,21 @@ local opt = vim.opt
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+
+if vim.env.SSH_TTY then
+  opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
